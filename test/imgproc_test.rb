2750966825637c041d722e95455c960ce7760799
Aaron Marburg <aaron.marburg@pg.canterbury.ac.nz>
2014-06-30 21:34:22 +1200

    Added some rough Canny and Hough transform tests.

--- test/imgproc_test.rb
+++ test/imgproc_test.rb
@@ -18,6 +18,26 @@ class TestImgproc < Minitest::Test
     assert_equal 1, grey.channels
     assert_equal img.depth, grey.depth
     assert_equal img.size, grey.size
+
+    save_image "/tmp/grey.jpg", grey
+  end
+
+  def test_canny
+    img = load_image test_image
+    grey = cvtcolor( img, Convert::BGR2GRAY )
+    edges = canny grey, 1.0, 2.0
+
+    save_image "/tmp/canny.jpg", edges
+  end
+
+  def test_houghlines
+    img = load_image test_image
+    grey = cvtcolor( img, Convert::BGR2GRAY )
+    edges = canny grey, 1.0, 2.0
+
+    lines = houghlines edges, 2, Math::PI/20, 2
+    lines.print('lines')
+    puts lines.type
   end
 
 end
